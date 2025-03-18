f854ad022b1da89250d4e1b156cb9903e7c57e31
Doonminus2 <piezapieza@email.com>
2025-03-18 14:43:43 +0700

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,7 +1,7 @@
 // ตัวอย่างโค้ดที่ไม่เป็นไปตามกฎของ ESLint และ Prettier
 function sayHello(name) {
-  console.log("Hello, " + name); // ❌ ควรใช้ template literals และมี semicolon
-  return "Hi, " + name;
+  console.log('Hello, ' + name) // ❌ ควรใช้ template literals และมี semicolon
+  return 'Hi, ' + name
 }
 
-sayHello("World");
+sayHello("World")
